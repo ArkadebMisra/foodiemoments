4cf6998168bd12f238a6d53bd2a52b329699fa37
ArkadebMisra <arkadebmisra@gmail.com>
2019-06-22 18:26:40 +0530

    digitalocean-2

--- foodiemoments/settings.py
+++ foodiemoments/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*_=mw^(6@58$m--5v2x#fgb!h_7d4q*d2g-khcw(f)d&6fi5gq'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['139.59.60.193','foodiemoments.com',localhost','127.0.0.1',]
+ALLOWED_HOSTS = ['139.59.60.193','foodiemoments.com','localhost','127.0.0.1',]
 
 
 # Application definition
