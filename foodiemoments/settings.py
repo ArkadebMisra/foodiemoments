7eae6dacf258a511837787078a58c082e36474ad
ArkadebMisra <arkadebmisra@gmail.com>
2019-06-23 10:00:04 +0530

    settings

--- foodiemoments/settings.py
+++ foodiemoments/settings.py
@@ -147,12 +147,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+STATICFILES_DIRS = [..DIRS..]
 
 #MY_SETTINGS
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, '/media/')
 
 from django.urls import reverse_lazy
 
