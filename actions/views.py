7e56123f36dcc886a5b6252a259f4f1b604b2b07
ArkadebMisra <arkadebmisra@gmail.com>
2019-06-24 10:54:39 +0530

    notifications

--- actions/views.py
+++ actions/views.py
@@ -9,11 +9,9 @@ def notification(request):
     following_ids = request.user.following.values_list('id',
                                                        flat=True)
 
-    images = Image.objects.all()
-    images_ids = images.filter(user=request.user).values_list('id')
     if following_ids:
         # If user is following others, retrieve only their actions
-        actions = actions.filter(target_id__in=images_ids)
+        actions = actions.filter(user_id__in=followings_ids)
     actions = actions.select_related('user', 'user__profile')\
                      .prefetch_related('target')[:10]
     return render(request,'actions/notification.html',{'actions':actions,
