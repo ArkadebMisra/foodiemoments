9dd112cfeacc4580a741e304ae0ec48c4a087876
ArkadebMisra <arkadebmisra@gmail.com>
2019-06-24 14:15:30 +0530

    bell

--- actions/views.py
+++ actions/views.py
@@ -6,16 +6,15 @@ from foods.models import Image
 
 def notification(request):
     # Display all actions by default
-    actions = Action.objects.exclude(user=request.user)
     following_ids = request.user.following.values_list('id',
                                                        flat=True)
 
     images = Image.objects.all()
     images_ids = images.filter(user=request.user).values_list('id',flat=True)
-    if following_ids:
-        # If user is following others, retrieve only their actions
-        actions = actions.filter(target_id__in=images_ids)
+    actions = Action.objects.exclude(user=request.user)
+    actions = actions.filter(target_id__in=images_ids)
     actions = actions.select_related('user', 'user__profile')\
                      .prefetch_related('target')[:10]
+                     
     return render(request,'actions/notification.html',{'actions':actions,
                                                        'images_ids':images_ids})
