0fb39ab609d606d785e6baefd69ca0f835a1a436
ArkadebMisra <arkadebmisra@gmail.com>
2019-06-24 11:09:33 +0530

    notification

--- actions/views.py
+++ actions/views.py
@@ -1,3 +1,4 @@
+
 from django.shortcuts import render
 from .models import Action
 from foods.models import Image
@@ -9,9 +10,12 @@ def notification(request):
     following_ids = request.user.following.values_list('id',
                                                        flat=True)
 
+    images = Image.objects.all()
+    images_ids = images.filter(user=request.user).values_list('id',flat=True)
     if following_ids:
         # If user is following others, retrieve only their actions
-        actions = actions.filter(user_id__in=followings_ids)\
-                         .select_related('user', 'user__profile')\
-                         .prefetch_related('target')[:10]
-    return render(request,'actions/notification.html',{'actions':actions})
+        actions = actions.filter(target_id__in=images_ids)
+    actions = actions.select_related('user', 'user__profile')\
+                     .prefetch_related('target')[:10]
+    return render(request,'actions/notification.html',{'actions':actions,
+                                                       'images_ids':images_ids})
